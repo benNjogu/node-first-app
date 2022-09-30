43ffa1bd00549597c9e6d264aa66894547bceeb4
benNjogu <kamandemachafi@gmail.com>
2022-09-30 11:07:52 +0300

    Module wrapper function part 1

--- logger.js
+++ logger.js
@@ -1,8 +1,13 @@
-var url = "http://mylogger.io/log";
+(function (exports, require, module, __filename, __dirname) {
+  console.log(__filename);
+  console.log(__dirname);
 
-function log(message) {
-  //send an HTTP req
-  console.log(message);
-}
+  var url = "http://mylogger.io/log";
 
-module.exports = log;
+  function log(message) {
+    //send an HTTP req
+    console.log(message);
+  }
+
+  module.exports = log;
+});
