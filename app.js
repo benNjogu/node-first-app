96980117d35ef71d3fbcea7cfc44bdbb9754066d
benNjogu <kamandemachafi@gmail.com>
2022-09-30 12:14:19 +0300

    Event arguments

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on("messageLogged", () => console.log("Listener called")); //-> Listener called
+emitter.on("messageLogged", (arg) => console.log("Listener called", arg)); //-> Listener called { id: 1, url: 'http://' }
 
 //Raise an event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
