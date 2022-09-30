95fb68a61040dc82500192430334b95066632c22
benNjogu <kamandemachafi@gmail.com>
2022-09-30 11:10:32 +0300

    Module wrapper function part 2

--- logger.js
+++ logger.js
@@ -1,13 +1,11 @@
-(function (exports, require, module, __filename, __dirname) {
-  console.log(__filename);
-  console.log(__dirname);
+console.log(__filename); //-> C:\Users\User\Documents\REPOS\node-first-app\logger.js
+console.log(__dirname); //-> C:\Users\User\Documents\REPOS\node-first-app
 
-  var url = "http://mylogger.io/log";
+var url = "http://mylogger.io/log";
 
-  function log(message) {
-    //send an HTTP req
-    console.log(message);
-  }
+function log(message) {
+  //send an HTTP req
+  console.log(message);
+}
 
-  module.exports = log;
-});
+module.exports = log;
