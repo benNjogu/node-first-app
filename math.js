bf65cb61bdfb82be946478611cc09cbdff1bc2b6
benNjogu <kamandemachafi@gmail.com>
2022-10-01 11:33:03 +0300

    Publishing own package

---
+++ math.js
@@ -0,0 +1,6 @@
+var math = require("math-for-kids");
+
+var result = math.add(2, 3);
+var result2 = math.multiply(4, 5);
+console.log("addition = " + result);
+console.log("multiplication = " + result2);
